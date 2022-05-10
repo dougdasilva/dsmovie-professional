944c538dfece928c6fac32fbc5464f6854db4299
dougdasilva <douglas_wfs@hotmail.com>
2022-05-10 09:59:36 -0300

    Projeto criado

---
+++ backend/src/test/java/com/dvsuperior/dsmovieprofessional/DsmovieProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvsuperior.dsmovieprofessional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
